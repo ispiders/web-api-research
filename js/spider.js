9536d6e72f7b481965cb4c62cea7627a385de5a8
Kyle Young <ispiders@163.com>
2018-06-15 12:04:42 +0800

    spider.ts

--- js/spider.js
+++ js/spider.js
@@ -1,117 +1,69 @@
-function readURL (url: string, encoding: string = 'utf-8'): Promise<string> {
-
+"use strict";
+function readURL(url, encoding = 'utf-8') {
     return fetch(url).then((response) => {
-
         return response.blob().then((blob) => {
-
             return readBlobText(blob, encoding);
         });
     });
 }
-
-function readBlobText (blob: Blob, encoding: string): Promise<string> {
-
+function readBlobText(blob, encoding) {
     let fr = new FileReader;
-
     return new Promise((resolve, reject) => {
-
         fr.onload = function () {
             resolve(fr.result);
         };
-
         fr.onerror = function (e) {
             reject(e);
         };
-
         fr.readAsText(blob, encoding);
     });
 }
-
-function parseHTML (text: string): HTMLDocument {
-
+function parseHTML(text) {
     let parser = new DOMParser;
-
     return parser.parseFromString(text, 'text/html');
 }
-
-function download (text: string, name: string = 'download.txt'): void {
-
+function download(text, name = 'download.txt') {
     let file = new File([text], name);
     let a = document.createElement('a');
-
     a.download = name;
     a.href = URL.createObjectURL(file);
-
     a.click();
 }
-
-interface TTaskParser<T=any> {
-    <T1 extends T>(spider: Spider, doc: HTMLDocument, task: T1): void;
-}
-
-interface Task {
-
-    url: string;
-    encoding?: string;
-
-    [key: string]: any;
-
-    parse: (spider: Spider, doc: HTMLDocument) => void;
-}
-
 class Spider {
-
-    tasks: Task[];
-    state: any[];
-    index: number = -1;
-    interval: number = 100;
-    paused: boolean = false;
-
-    constructor (tasks: Task[]) {
+    constructor(tasks) {
+        this.index = -1;
+        this.interval = 100;
+        this.paused = false;
         this.tasks = tasks;
         this.state = [];
     }
-
-    addTask (task: Task): void {
-
+    addTask(task) {
         this.tasks.push(task);
     }
-
-    next (): Task | undefined {
-
+    next() {
         let len = this.tasks.length;
-
         if (len) {
             this.index++;
-
             return this.tasks.shift();
         }
-
         return undefined;
     }
-
-    getDocument (url: string, encoding: string = 'utf-8'): Promise<HTMLDocument> {
+    getDocument(url, encoding = 'utf-8') {
         return readURL(url, encoding)
             .then((text) => {
-                return parseHTML(text);
-            });
+            return parseHTML(text);
+        });
     }
-
-    pause () {
+    pause() {
         this.paused = true;
     }
-
-    run (): void {
-
+    run() {
         let task = this.next();
-
         if (task) {
             this.getDocument(task.url, task.encoding).then((doc) => {
-
                 if (task) {
                     task.parse(this, doc);
                 }
-
                 if (!this.paused) {
                     setTimeout(() => {
                         this.run();
@@ -121,20 +73,10 @@ class Spider {
         }
     }
 }
-
-interface TMenuTask extends Task {
-    menuSelector: string;
-    contentSelector: string;
-}
-
-function parseMenu (this: TMenuTask, spider: Spider, doc: HTMLDocument): void {
-
-    let elems: NodeListOf<HTMLLinkElement> = doc.querySelectorAll(this.menuSelector);
-
+function parseMenu(spider, doc) {
+    let elems = doc.querySelectorAll(this.menuSelector);
     for (let i = 0; i < elems.length; ++i) {
-
-        let item: HTMLLinkElement = elems[i];
-
+        let item = elems[i];
         spider.addTask({
             url: item.href,
             title: item.innerText,
@@ -143,23 +85,18 @@ function parseMenu (this: TMenuTask, spider: Spider, doc: HTMLDocument): void {
         });
     }
 }
-
-function parseContent (this: TMenuTask, spider: Spider, doc: HTMLDocument) {
-
-    let el = doc.querySelector(this.contentSelector) as HTMLElement;
-
+function parseContent(spider, doc) {
+    let el = doc.querySelector(this.contentSelector);
     spider.state.push({
         url: this.url,
         title: this.title,
         content: el.innerText
     });
 }
-
-let tasks: Task[] = [{
-    url: './',
-    menuSelector: '#list a',
-    contentSelector: '#content',
-    parse: parseMenu
-}];
-
+let tasks = [{
+        url: './',
+        menuSelector: '#list a',
+        contentSelector: '#content',
+        parse: parseMenu
+    }];
 let spider = new Spider(tasks);
