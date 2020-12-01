936fb816156e8160f560abe9d2317da96912e124
Kyle Young <ispiders@163.com>
2020-12-01 19:59:06 +0800

    -

--- web-spider/spiders/dishen.ts
+++ web-spider/spiders/dishen.ts
@@ -25,10 +25,13 @@ spider = (function () {
     type Spi = typeof spider;
 
     // 起名任务入口
-    spider.addTask('https://www.dishen.com/qm/bbqm/', {}, {topcate: 'qm', root: true});
+    // spider.addTask('https://www.dishen.com/qm/bbqm/', {}, {topcate: 'qm', root: true});
 
     // 星座入口
-    spider.addTask('https://www.dishen.com/xz/love/', {}, {topcate: 'xz', root: true});
+    // spider.addTask('https://www.dishen.com/xz/love/', {}, {topcate: 'xz', root: true});
+
+    // 解梦入口
+    spider.addTask('https://www.dishen.com/jm/rw/', {}, {topcate: 'jm', root: true});
 
     // 分类入口
     spider.addRule({
@@ -37,7 +40,7 @@ spider = (function () {
         },
         parse: (spider: Spi, doc: HTMLDocument, task: Task) => {
 
-            let links = [...doc.querySelectorAll<HTMLAnchorElement>('.list-hd-innerwrap a[href]')];
+            let links = [...doc.querySelectorAll<HTMLAnchorElement>('.list-hd-content a[href]')];
 
             links.forEach((a) => {
                 let path = a.href.replace(/^\/|\/$/g, '');
@@ -159,7 +162,7 @@ spider = (function () {
                 date: date.getTime()
             });
 
-            if (date.getFullYear() < 2020) {
+            if (date.getFullYear() < 2020 && task.data.topcate !== 'jm') {
                 spider.state.finishedCate[task.data.topcate + '/' + task.data.cate] = 1;
             }
         }
@@ -167,6 +170,156 @@ spider = (function () {
 
     spider.cache = true;
     spider.run();
+
+    function genImportSql (cates: ICate[], articles: IArticle[], limit: number = 0) {
+        let cateidAutoIncrement = 300;
+        let articleAutoIncrement = 20000;
+
+        const topcateIdMap = {
+            'xz': 18,
+            'qm': 19,
+            'jm': 25,
+        };
+
+        const cateMap: {[alias: string]: ICate} = {};
+        const cateIdMap = cates.reduce((ret, cate) => {
+
+            let catid = cateidAutoIncrement++;
+
+            ret[cate.alias] = catid;
+            cateMap[cate.alias] = cate;
+
+            return ret;
+        }, {} as {[alias: string]: number});
+
+        function mapCateData (cate: ICate) {
+
+            let catid = cateIdMap[cate.alias];
+            let parentid = topcateIdMap[cate.topcate || 'qm'];
+
+            return {
+                catid: catid,
+                catname: cate.title,
+                catdir: cate.alias,
+                arrparentid: ['0', parentid, catid].join(','),
+                arrchildid: String(catid),
+                modelid: 1,
+                parentid: parentid,
+                category_template: 'category_article', // 'category_jm',
+                list_template: 'list_article',
+                show_template: 'show_article',
+                pclink: '/' + cate.alias + '/'
+            };
+        }
+
+        function mapCateId (alias: string) {
+            return cateIdMap[alias];
+        }
+
+        function getDescription (html: string, len: number = 100) {
+
+            let doc = parseHTML(html);
+
+            return doc.body.innerText.slice(0, len);
+        }
+
+        function mapArticleData (article: IArticle) {
+
+            let id = articleAutoIncrement++;
+            let cate = cateMap[article.cate];
+            let date = Math.floor((article.date || Date.now()) / 1000) + parseInt(article.id);
+
+            return {
+                id: id,
+                catid: mapCateId(article.cate),
+                articledir: cate.alias,
+                url: '/' + cate.alias + '/' + id + '.html',
+                userid: 1,
+                username: 'admin',
+                nickname: '管理员',
+                title: article.title,
+                description: getDescription(article.content),
+                inputtime: date,
+                updatetime: date,
+                content: article.content,
+                copyfrom: '原创',
+                thumb: article.img || ''
+            };
+        }
+
+        function genInsertSql (table: string, data: any[], packetLimit: number = 1000) {
+
+            let columns = Object.keys(data[0]);
+            let sqlhead = `insert into \`${table}\` ` + ' (' + columns.map((name) => '`' + name + '`').join(',') + ') values \n';
+            let packet: string[] = [];
+
+            let rows: string[] = [];
+
+            data.forEach((row) => {
+
+                if (packet.length >= packetLimit) {
+                    rows.push(sqlhead + packet.join(',\n') + ';');
+                    packet = [];
+                }
+
+                packet.push('(' + columns.map((col) => {
+                    let field = row[col];
+                    return JSON.stringify(field);
+                }).join(',') + ')');
+            });
+
+            if (packet.length) {
+                rows.push(sqlhead + packet.join(',\n') + ';');
+            }
+
+            return rows.join('\n');
+        }
+
+        function genCateSql (cates: ICate[]) {
+
+            let categroup: {[key: string]: number[]} = {};
+            let parentids = unique(cates.reduce((ret, cate) => {
+
+                let parentid: number = topcateIdMap[cate.topcate || 'qm'];
+
+                ret.push(parentid);
+
+                if (!categroup[parentid]) {
+                    categroup[parentid] = [mapCateId(cate.alias)];
+                }
+                else {
+                    categroup[parentid].push(mapCateId(cate.alias));
+                }
+
+                return ret;
+            }, [] as number[]));
+
+            return [
+                'delete from `yzm_category` where parentid in (' + parentids.join(',') + ');',
+                genInsertSql('yzm_category', cates.map((item) => mapCateData(item))),
+                parentids.map((parentid) => {
+                    return [
+                        'update `yzm_category` set `arrchildid` = "' + categroup[parentid].join(',') + '" where catid=' + parentid + ';',
+                        'delete from `yzm_article` where `catid` in (' + categroup[parentid].join(',') + ');'
+                    ].join('\n');
+                })
+            ].join('\n');
+        }
+
+        function genArticleSql (data: IArticle[]) {
+
+            data = data.sort((a, b) => a.id - b.id);
+
+            return genInsertSql('yzm_article', data.map((item) => mapArticleData(item)));
+        }
+
+        let ret = [
+            genCateSql(cates),
+            genArticleSql(articles),
+        ].join('\n');
+
+        download(ret, 'dishen-article-import.sql');
+    }
 
     return spider;
 }());
--- web-spider/spiders/shensuantang.ts
+++ web-spider/spiders/shensuantang.ts
@@ -123,59 +123,125 @@ spider = (function () {
     spider.cache = true;
     spider.run();
 
+    function importJm (limit: number = 0) {
+        let cateidAutoIncrement = 200;
+        let articleAutoIncrement = 1;
+
+        const newCatesMap = {
+            1: {title: "人物", alias: 'renwu'},
+            2: {title: "动物", alias: 'dongwu'},
+            3: {title: "植物", alias: 'zhiwu'},
+            4: {title: "物品", alias: 'wupin'},
+            5: {title: "身体", alias: 'shenti'},
+            6: {title: "生活", alias: 'shenghuo'},
+            7: {title: "活动", alias: 'huodong'},
+            8: {title: "自然", alias: 'ziran'},
+            9: {title: "情爱", alias: 'qingai'},
+            10: {title: "鬼神", alias: 'guishen'},
+            11: {title: "建筑", alias: 'jianzhu'},
+            12: {title: "其它", alias: 'qita'},
+        };
+
+        const newCates = Object.keys(newCatesMap).map((id) => {
+
+            return newCatesMap[id];
+        });
+
+        function mapCateData (data) {
+
+            let catid = cateidAutoIncrement++;
+
+            data.catid = catid;
+
+            return {
+                catid: catid,
+                catname: data.title,
+                catdir: data.alias,
+                arrparentid: '0,25,' + catid,
+                arrchildid: String(catid),
+                modelid: 1,
+                parentid: 25,
+                category_template: 'category_jm', // 'category_article',
+                list_template: 'list_article',
+                show_template: 'show_article',
+                pclink: '/' + data.alias + '/'
+            };
+        }
+
+        function mapCateId (oid: number) {
+            return newCatesMap[oid].catid;
+        }
+
+        function getDescription (html: string, len: number = 100) {
+
+            let div = document.createElement('div');
+            div.innerHTML = html;
+            return div.innerText.slice(0, len);
+        }
+
+        function mapArticleData (data: Jm) {
+
+            let id = articleAutoIncrement++;
+            let cate = newCatesMap[data.cate];
+
+            return {
+                id: id,
+                catid: mapCateId(data.cate),
+                url: '/' + cate.alias + '/' + id + '.html',
+                userid: 1,
+                username: 'admin',
+                nickname: '管理员',
+                title: data.title,
+                inputtime: Math.floor(Date.now() / 1000),
+                updatetime: Math.floor(Date.now() / 1000),
+                description: getDescription(data.content),
+                content: data.content,
+                copyfrom: '原创',
+                thumb: data.img || ''
+            };
+        }
+
+        function genInsertSql (table: string, data: any[], limit: number = 0) {
+
+            let columns = Object.keys(data[0]);
+
+            if (limit) {
+                data = data.slice(0, limit);
+            }
+
+            let rows = data.map((row) => {
+                return '(' + columns.map((col) => {
+                    let field = row[col];
+                    return typeof field === 'string' ? "'" + field.replace(/\'/g, '\\\'') + "'" : field;
+                }).join(',') + ')';
+            }).join(',\n');
+
+            let sql = `insert into \`${table}\` `;
+
+            return sql + ' (' + columns.map((name) => '`' + name + '`').join(',') + ') values \n' + rows + ';';
+        }
+
+        function genCateSql (data: any[]) {
+
+            return genInsertSql('yzm_category', data.map((item) => mapCateData(item)));
+        }
+
+        function genArticleSql (data: Jm[]) {
+
+            return genInsertSql('yzm_article', data.map((item) => mapArticleData(item)), limit);
+        }
+
+        let ret = [
+            'delete from `yzm_category` where parentid=25;',
+            genCateSql(newCates),
+            'update yzm_category set `arrchildid` = "' + newCates.map(cate => cate.catid).join(',') + '" where catid=25;',
+            'delete from `yzm_article` where id > 0 and id < 20000;',
+            genArticleSql(spider.state.jms),
+        ].join('\n');
+
+        download(ret, 'jm-import.sql');
+    }
+
     return spider;
 }());
 
-function importJm () {
-    let cateidAutoIncrement = 200;
-
-    const newCates = {
-        1: {title: "人物", alias: 'renwu'},
-        2: {title: "动物", alias: 'dongwu'},
-        3: {title: "植物", alias: 'zhiwu'},
-        4: {title: "物品", alias: 'wupin'},
-        5: {title: "身体", alias: 'shenti'},
-        6: {title: "生活", alias: 'shenghuo'},
-        7: {title: "活动", alias: 'huodong'},
-        8: {title: "自然", alias: 'ziran'},
-        9: {title: "情爱", alias: 'qingai'},
-        10: {title: "鬼神", alias: 'guishen'},
-        11: {title: "建筑", alias: 'jianzhu'},
-        12: {title: "其它", alias: 'qita'},
-    };
-
-    function mapCateData (data) {
-
-        return {
-            catid: cateidAutoIncrement++,
-            catname: data.title,
-            catdir: data.alias,
-            modelid: 1,
-            parentid: 25,
-            category_template: 'category_article',
-            list_template: 'list_article',
-            show_template: 'show_article',
-        };
-    }
-
-    function genCateRow (data: any[]) {
-
-        let columns = Object.keys(data[0]);
-
-        let rows = data.map((row) => {
-            return '(' + columns.map((col) => {
-                let field = row[col];
-                return typeof field === 'string' ? "'" + field.replace(/\'/g, '\\\'') + "'" : field;
-            }).join(',') + ')';
-        }).join(',\n');
-
-        let sql = 'insert into yzm_category';
-
-        return sql + ' (' + columns.map((name) => '`' + name + '`').join(',') + ') values \n' + rows;
-    }
-
-    function genCateSql (data: any[]) {
-
-        return genCateRow(data.map((item) => mapCateData(item)));
-    }
-}
